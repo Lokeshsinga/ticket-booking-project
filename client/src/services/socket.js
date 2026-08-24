7bcdfdf2d87ec62730ecd81ad2416c32cabb9dd5
Lokeshsinga <lokeshsinga2622gmail.com>
2026-08-24 15:26:15 +0530

    Fix production Socket.IO URL

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -1,10 +1,8 @@
 import { io } from 'socket.io-client';
 
-const apiUrl =
-  import.meta.env.VITE_API_URL ||
-  'http://localhost:4000/api';
-
-const socketUrl = apiUrl.replace(/\/api\/?$/, '');
+const socketUrl =
+  import.meta.env.VITE_SOCKET_URL ||
+  'https://ticket-booking-project-t85i.onrender.com';
 
 export const socket = io(socketUrl, {
   autoConnect: false,
